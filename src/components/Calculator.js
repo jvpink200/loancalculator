4b04438f14aa2efc7c59b3e21c3225651473003e
jvpink200 <jvillarr20@gmail.com>
2021-08-06 13:50:10 -0700

    changed calculator

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,33 +1,53 @@
 import React, { useState } from "react";
 
 function Calculator() {
-    const [add, setAdd] = useState(0);
-    const [result, setResult] = useState(add);
+    const [userValues, setUserValues] = useState({
+      amount:'',
+      interest:'',
+      years: ''
+    });
+    function handleResetClick() {
+      setUserValues('');
+      console.log(userValues);
+      console.log(setUserValues);
+    }
     return (
       <React.Fragment>
         <div className="row mt-5 text-center">
           <div className="result pt-4">Result:</div>
+          <div className="reset pt-4" onClick={handleResetClick}>
+            Reset
+          </div>
         </div>
-        <div className="row text-center">
-          <div className="square pt-4">1</div>
-          <div className="square pt-4">2</div>
-          <div className="square pt-4">3</div>
-          <div className="add-square pt-4">+</div>
-        </div>
-        <div className="row text-center">
-          <div className="square pt-4">4</div>
-          <div className="square pt-4">5</div>
-          <div className="square pt-4">6</div>
-          <div className="subtract-square pt-4">-</div>
-        </div>
-        <div className="row text-center">
-          <div className="square pt-4">7</div>
-          <div className="square pt-4">8</div>
-          <div className="square pt-4">9</div>
-          <div className="equal-square pt-4">=</div>
-        </div>
+        <form>
+          <div className="row text-center">
+            <div className="square pt-4">
+              <label>Amount</label>
+              <input
+                type="text"
+                value={userValues.amount}
+                onChange={handleInputChange}
+              />
+            </div>
+            <div className="square pt-4">
+              <input
+                type="text"
+                value={userValues.interest}
+                onChange={handleInputChange}
+              />
+            </div>
+            <div className="square pt-4">
+              <input
+                type="text"
+                value={userValues.years}
+                onChange={handleInputChange}
+              />
+            </div>
+          </div>
+        </form>
       </React.Fragment>
     );
 }
+
 
 export default Calculator;
